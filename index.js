c0b488b737f4497617459a972a56db259dc23c40
priscillahuynh <priscillamhuynh@gmail.com>
2021-03-23 13:49:28 +0400

    commit tree 6d2cd927d9bf0955a4f1d397e44f2b2d36607c56

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1*num2
 
-// Write your code here
+const random = Math.ceil( Math.random() * 100 )
+
+
+const num3 = 14
+const num4 = 5
+const mod = num3%num4
+const max = Math.max( 5, 10, 15, 20 )
